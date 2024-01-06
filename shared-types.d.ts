365f8c574bb293411ce155d1554b964ce0fb7eb2
Mateusz Sosnowski <sosnowskimateusz25@gmail.com>
2024-01-06 13:10:32 +0100

    Adding types
    Adding types

---
+++ shared-types.d.ts
@@ -0,0 +1,12 @@
+export interface CreateAccount {
+    name: string;
+    currency: string;
+    balance?: number;
+}
+
+export interface GetAccount {
+    id: number;
+    name: string;
+    balance: number;
+    currency: string;
+}
