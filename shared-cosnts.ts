696873b6d42ef7ffd8b05fd473ec447b1043d3e2
Mateusz Sosnowski <sosnowskimateusz25@gmail.com>
2024-01-11 18:00:03 +0100

    Adding transactions categories

---
+++ shared-cosnts.ts
@@ -0,0 +1,13 @@
+export enum TransactionsCategories {
+    HOUSING = 'Housing',
+    UTILITIES = 'Utilities',
+    TRANSPORTATION = 'Transportation',
+    FOOD_AND_DRINKS = 'Food and drinks',
+    HEALTH = 'Health',
+    ENTERTAINMENT = 'Entertainment',
+    PERSONAL_CARE = 'Personal Care',
+    DEBT_REPAYMENT = 'Debt Repayment',
+    EDUCATION = 'Education',
+    TAXES = 'Taxes',
+    MISCELLANEOUS = 'Miscellaneous',
+}
