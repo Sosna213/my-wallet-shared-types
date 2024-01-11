1acc44833b189c010207044a64de42525a5b2117
Mateusz Sosnowski <sosnowskimateusz25@gmail.com>
2024-01-11 18:24:55 +0100

    Addning transactions types

--- shared-types.d.ts
+++ shared-types.d.ts
@@ -1,3 +1,5 @@
+import {TransactionsCategories} from "../../my-wallet-api/my-wallet-shared-types/shared-cosnts";
+
 export interface CreateAccount {
     name: string;
     currency: string;
@@ -15,4 +17,29 @@ export interface RegisterUser {
     nickname: string;
     email: string;
     name: string;
+}
+
+export interface CreateTransactionDTO {
+    name: string;
+    amount: number;
+    category: TransactionsCategories;
+    date: Date
+    accountId: string
+}
+
+export interface GetTransactionDTO {
+    name: string;
+    amount: number;
+    category: TransactionsCategories;
+    date: Date
+    accountId: string
+    userId: string
+}
+
+export interface TransactionsFilters {
+    accountId?: string,
+    category?: TransactionsCategories,
+    eq?: number,
+    gte?: number,
+    lte?: number,
 }
