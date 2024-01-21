21adc49d60f654482469f8ec5e2bca4b54df62e1
Mateusz Sosnowski <sosnowskimateusz25@gmail.com>
2024-01-21 18:44:35 +0100

    Currency update

--- shared-types.d.ts
+++ shared-types.d.ts
@@ -43,10 +43,10 @@ export interface TransactionsFilters {
     transactionName?: string,
     accountId?: string[],
     category?: string[],
+    currency?: string[],
     eq?: number,
     gte?: number,
     lte?: number,
     fromDate?: string,
     toDate?: string,
-    currency?: string,
 }
